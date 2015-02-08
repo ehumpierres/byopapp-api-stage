f72f58551912f183041e6784106127917e7c20e1
ehumpierres <ehumpierres@Ernestos-MacBook-Air-2.local>
2015-02-07 19:31:02 -0500

    testgit status

--- services/dashboard/dashboard.py
+++ services/dashboard/dashboard.py
@@ -136,7 +136,7 @@ def get_unread_notifications(user_id = None):
         print traceback.format_exc()
         abort(500)
 
-@dashboard_api.route('/mandrillreplies', methods=['POST']) #Here i will make some changes
+@dashboard_api.route('/mandrillreplies', methods=['POST']) #Here i will make some changes tetete
 def save_received_user_mandrill_email():
     """
     print "save_received_user_mandrill_email"
